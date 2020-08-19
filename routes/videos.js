5e621dea941ed873453dc481f5671d452c93e49c
KozubIgn <ignacykozub@gmail.com>
2020-08-19 18:41:54 +0200

    update video refactor

--- routes/videos.js
+++ routes/videos.js
@@ -2,6 +2,7 @@ const express = require("express");
 const promises = require("../videosPromise");
 const router = express.Router();
 
+
 //There should be random 5 videos on home page
 router.get("/", async (req, res) => {
     const arr = await promises.getVideosPromise().then(value => {
@@ -13,18 +14,36 @@ router.get("/", async (req, res) => {
 })
     
 // UPDATE
-router.get('/video/:id', async (req, res) => {
-    const result = await promises.getIdPromise(req.params.id).then(value => {
-        return value;
-
-    })
-    console.log(result);
-    res.render("updateForm", {
-        title: result[0].title,
-        description: result[0].description,
-        tags: result[0].tags,
-        url: result [0].url
-    })
+router.put('/video/:id', function (req, res) {
+    const newVideo = {
+        title: req.body.title,
+        description: req.body.description,
+        tags: req.body.tags,
+        url: req.body.url
+    }
+    pool.query('UPDATE videos SET ($1, $2, $3, $4) WHERE id ='+ req.params.id ,
+        [newVideo.title, newVideo.description, newVideo.tags, newVideo.url], (err, results) => {
+            if (err) {
+                throw err;
+            }
+            res.status(201).send('Video updated !')
+            console.log(newVideo);
+        });
 })
+    router.get('/video/:id', async (req, res) => {
+        const result = await promises.getIdPromise(req.params.id).then(value => {
+            return value;
+
+        })
+        console.log(result);
+        res.render("updateForm", {
+            id: result[0].id,
+            title: result[0].title,
+            description: result[0].description,
+            tags: result[0].tags,
+            url: result [0].url
+        })
+    })
+
 // console.log(arr);
 module.exports = router;
